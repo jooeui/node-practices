dd81c12c93fc5b2a7fc7aa4a86d1d705023590af
jooeui <kje_0727@naver.com>
2021-11-15 12:33:44 +0900

    first commit

---
+++ douzone-steamcare-math/douzone-steamcare-math.js
@@ -0,0 +1,26 @@
+moduel.exports = {
+    sum: function(){
+        var sum = 0;
+        Array.from(arguments).forEach(function(e) { // 유사 배열을 배열로
+            sum += e;
+        });
+    
+        return sum;
+    },
+    max: function() {
+        var max = Number.MIN_SAFE_INTEGER;
+        Array.from.forEach(function(e){
+            max = e > max ? e : max;
+        });
+
+        return max;
+    },
+    min: function() {
+        var min = Number.MAX_SAFE_INTEGER;
+        Array.from(arguments).forEach(function(e){
+            min = e < min ? e : min;
+        });
+
+        return min;
+    }
+}
