5624aa746eec31403e43bc3bcb29f5adbe6e4e09
jooeui <kje_0727@naver.com>
2021-11-16 16:03:35 +0900

    emaillist #3

--- emaillist/models/emaillist.js
+++ emaillist/models/emaillist.js
@@ -1,8 +1,8 @@
 const mysql = require('mysql2');
-
+const util = require('util');
 
 module.exports = {
-    findAll: function(){
+    findAll: async function(callback){
         const conn = mysql.createConnection({
             host: '127.0.0.1',
             port: 3306,
@@ -11,6 +11,28 @@ module.exports = {
             database: 'webdb'
         });
 
+
         // callback으로 말고 다르게 할 거래융
+        
+        // Promise를 return 하는 애는 mapping 해줘야 함
+        // const query = function(sql, data){
+        //     return new Promise(function(resolve, reject){
+        //         conn.query(sql, [], function(error, results, filed){
+        //             return error ? reject(error) : resolve(results);
+        //         })
+        //     })
+        // }
+        
+        // const query = (sql, data) => new Promise((resolve, reject) => conn.query(sql, [], (error, results, filed) => error ? reject(error) : resolve(results)));
+        
+        const query = util.promisify(conn.query).bind(conn);
+
+        try {
+            return await query("select no, first_name as firstName, last_name as lastName, email from emaillist order by no desc", []);
+        } catch(e) {
+            console.error(e);
+        } finally {
+            conn.end();
+        }
     }
 }
