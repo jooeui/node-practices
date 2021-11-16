07d3d5fe15799e3c05ea99d052666a16ed031a65
jooeui <kje_0727@naver.com>
2021-11-16 17:12:40 +0900

    emaillist #final

--- emaillist/controllers/emaillist.js
+++ emaillist/controllers/emaillist.js
@@ -3,9 +3,19 @@ const model = require('../models/emaillist');
 module.exports = {
     index: async function(req, res) {
         const results = await model.findAll(function(){});
-        console.log(results);
+        // console.log(results);
+
         res.render('index', {
             list: results || []
         });
+    },
+    form: function(req, res) {
+        res.render('form');
+    },
+    add: async function(req, res) {
+        console.log(req.body);
+        const results = await model.insert(req.body);
+
+        res.redirect("/");
     }
 }
--- emaillist/models/emaillist.js
+++ emaillist/models/emaillist.js
@@ -1,17 +1,11 @@
 const mysql = require('mysql2');
+const dbconn = require('./dbconn');
 const util = require('util');
 
 module.exports = {
     findAll: async function(callback){
-        const conn = mysql.createConnection({
-            host: '127.0.0.1',
-            port: 3306,
-            user: 'webdb',
-            password: 'webdb',
-            database: 'webdb'
-        });
-
-
+        const conn = dbconn();
+        
         // callback으로 말고 다르게 할 거래융
         
         // Promise를 return 하는 애는 mapping 해줘야 함
@@ -28,7 +22,25 @@ module.exports = {
         const query = util.promisify(conn.query).bind(conn);
 
         try {
-            return await query("select no, first_name as firstName, last_name as lastName, email from emaillist order by no desc", []);
+            return await query(
+                "select no, first_name as firstName, last_name as lastName, email from emaillist order by no desc", 
+                []
+            ); // (dbquery, binding 할 데이터)
+        } catch(e) {
+            console.error(e);
+        } finally {
+            conn.end();
+        }
+    },
+    insert: async function(emaillist) {
+        const conn = dbconn();
+        const query = util.promisify(conn.query).bind(conn);
+
+        try {
+            return await query(
+                "insert into emaillist(first_name, last_name, email) values(?, ?, ?)",
+                Object.values(emaillist)
+            );
         } catch(e) {
             console.error(e);
         } finally {
